9a4b9cedf0d0d26eb7163c657c0a4ea7cd64c122
Zoe Punjwalni <zoepunjwani@gmail.com>
2019-06-19 11:13:09 -0400

    Initial Save

---
+++ AviatrixApp/Aviatrix.swift
@@ -0,0 +1,32 @@
+//
+//  Aviatrix.swift
+//  AviatrixApp
+//
+//  Created by Amy Holt on 6/10/18.
+//  Copyright © 2018 Amy Holt. All rights reserved.
+//
+
+import Foundation
+
+class Aviatrix {
+    
+    func start() -> Bool {
+        return true
+    }
+    
+    func refuel() {
+        
+    }
+    
+    func flyTo(destination : String) {
+        
+    }
+    
+    func distanceTo(target : String) {
+    
+    }
+    
+    func knownDestinations() -> [String] {
+       return ["St. Louis"]
+    }
+}
